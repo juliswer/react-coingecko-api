4ff164223d75d84c17072e3b14f7e376583a3abb
Julian Swerdlin <julianswer36@gmail.com>
2022-01-08 20:46:47 +0100

    updating the searchbar

--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import CoinRow from './CoinRow';
 
-const TableCoins = ({coins}) => {
+const TableCoins = ({coins, search}) => {
 
     const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume']
+
+    const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) | coin.symbol.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <table className="table table-dark mt-4 table-hover">
@@ -15,7 +17,7 @@ const TableCoins = ({coins}) => {
                 </tr>
             </thead>
             <tbody>
-                {coins.map((coin, index) => (
+                {filteredCoins.map((coin, index) => (
                     <CoinRow coin={coin} key={index} index={index + 1} />
                 ))}
             </tbody>
