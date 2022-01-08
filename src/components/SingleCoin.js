17bb5804af39cfc02c48bcc113c00e81cb78f9e0
Julian Swerdlin <julianswer36@gmail.com>
2022-01-08 21:57:47 +0100

    detail page changes

--- src/components/SingleCoin.js
+++ src/components/SingleCoin.js
@@ -2,35 +2,15 @@ import React from 'react'
 
 const SingleCoin = ({coin}) => {
 
-    const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume']
-
     return (
-        <table className="table table-dark mt-4 table-hover">
-            <thead>
-                <tr>
-                    {titles.map((title, index) => (
-                        <td key={index}>{title}</td>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>{coin.index}</td>
-                <td>
-                    <img src={coin.image} alt={coin.name} className="img-fluid me-2" style={{width: '3%'}} />
-                    <span>
-                        {coin.name}
-                    </span>
-                    <span className="ms-2 text-muted text-uppercase">
-                        {coin.symbol}
-                    </span>
-                </td>
-                <td>{coin.current_price}</td>
-                <td className={coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'}>{coin.price_change_percentage_24h}</td>
-                <td>{coin.total_volume}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="card text-white bg-dark mt-2">
+            <div className="card-body">
+                <h5 className="card-title">{coin.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{coin.symbol}</h6>
+                <p className="card-text">{coin.description.en.slice(0,850)}...</p>
+                <a href={coin.links.homepage} className="card-link">View Crypto's page</a>
+            </div>
+        </div>
     )
 }
 
