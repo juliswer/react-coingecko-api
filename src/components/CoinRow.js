2bf60cef3ba31bbb9ee2a17ef53b0226f8eb7cdc
Julian Swerdlin <julianswer36@gmail.com>
2022-01-08 20:39:58 +0100

    updating rows

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -14,7 +14,7 @@ const CoinRow = ({coin, index}) => {
                 </span>
             </td>
             <td>{coin.current_price}</td>
-            <td>{coin.price_change_percentage_24h}</td>
+            <td className={coin.price_change_percentage_24h > 0 ? 'text-success' : 'text-danger'}>{coin.price_change_percentage_24h}</td>
             <td>{coin.total_volume}</td>
         </tr>
     )
