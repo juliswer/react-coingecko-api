cc5e9ca01c8acb3bda3e7cbefcaba3632b1f4eed
Julian Swerdlin <julianswer36@gmail.com>
2022-01-08 20:20:12 +0100

    using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import TableCoins from './components/TableCoins';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const getData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    setCoins(res.data)
+  }
+
+  useEffect( () => {
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <h1>Coin Market</h1>
+      <TableCoins coins={coins} />
     </div>
   );
 }
---
+++ src/components/TableCoins.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TableCoins = ({coins}) => {
+
+    console.log(coins)
+
+    return (
+        <div>
+            table
+        </div>
+    )
+}
+
+export default TableCoins
